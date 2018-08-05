6ed0de4b7a69dc347ef8b7d53908517ae65b0861
aboud20-meet <aboud20@meet.mit.edu>
2018-08-05 16:29:22 +0300

    back to python

--- exercises/for_loops.py
+++ exercises/for_loops.py
@@ -1,1 +1,46 @@
-# Write your solution for 1.1 here!
+# # Write your solution for 1.1 here!
+# # sum =0
+# # for i in range(1,101):
+# # 	if (i % 2 == 0):
+# # 		sum = sum + i
+# # 	else:
+# # 		i = sum + i
+# # print (sum)
+# # number = 0
+# # sum =0 
+# # while (sum < 10000):
+# # 	number = number +1
+# # 	sum  = sum  + number 
+# # print (number)
+
+# def is_prime(x):
+# 	check = True 
+# 	y =x 
+# 	while (x>0 and y>0):
+# 		if (x % y ==0):
+# 			y = y-1
+			
+# 		else:
+# 			print ("this isn't a prime number, sorry :(")
+# 			y = y-1
+# 			check = False 
+# 	return check 
+			
+
+
+
+# print(is_prime(6))
+
+#making superheros 
+class superheros :
+	def __init__(self, name, superpower,strength ):
+		self.name = name 
+		self.superpower = superpower
+		self.strength = int(strength)
+
+	def print_about(self):
+		print("my name is "+ self.name)
+		print ("my superpower is "+ self.superpower)
+		print("the amount of strength I have is "+ str(self.strength))
+superhero = superheros("Jenny","the cat lover", 7)
+superhero.print_about()
--- lab/part1.py
+++ lab/part1.py
@@ -1,9 +1,13 @@
 # Part 1 of the Python Review lab.
 
 def hello_world():
+	print("hello world")
 	pass
 
 def greet_by_name(name):
+	print("please enter your name")
+	name = input
+	print 
 	pass
 
 def encode(x):
